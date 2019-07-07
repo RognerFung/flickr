35562eb677e8d3e84a465d6b436d7197a8419bb3
xiaoyong.fang@nufindata.com <xiaoyong.fang@nufindata.com>
2019-07-08 01:13:04 +0800

    search for tags

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 3000
 const router = require('./router')
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
+app.disable('x-powered-by')
 app.use('/', router)
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
     console.error(err)
     res.json(err)
 })
--- router.js
+++ router.js
@@ -3,39 +3,38 @@ const cors = require('./cors')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const https = require('https')
-const parseString = require('xml2js').parseString
 router.use(bodyParser.json())
 
 router.route('/')
 .get(cors.cors, async (req, res, next) => {
+    https.get('https://api.flickr.com/services/feeds/photos_public.gne?format=json', result => {
+        let resultString = ''
 
-    https.get('https://api.flickr.com/services/feeds/photos_public.gne', (result) => {
-        let xml = ''
-    
-        result.on('data', (chunk) => {
-            xml += chunk
+        result.on('data', (data) => {
+            resultString += data
         })
-    
+
         result.on('end', () => {
-            parseString(xml, (err, result) => {
-                console.dir(result)
-                const resp = result.feed.entry.map(e => {
-                    return {
-                        title: e.title[0],
-                        link: e.link.find(el => el.$.type.includes('image')).$.href,
-                        published: e.published[0],
-                        updated: e.updated[0],
-                        author: e.author[0].name[0]
-                    }
-                })
-                res.json(resp)
-            })
+            const resultJson = JSON.parse(resultString.slice(15,-1))
+            res.json(resultJson)
         })
     })
 })
 .post(cors.cors, async (req, res, next) => {
     console.log(req.body)
-    res.json({status:'success'})
+    const tags = req.body.tags.replace(/\s/g, ',')
+    https.get(`https://api.flickr.com/services/feeds/photos_public.gne?tags=${tags}&format=json`, result => {
+        let resultString = ''
+
+        result.on('data', (data) => {
+            resultString += data
+        })
+
+        result.on('end', () => {
+            const resultJson = JSON.parse(resultString.slice(15,-1))
+            res.json(resultJson)
+        })
+    })
 })
 
 
